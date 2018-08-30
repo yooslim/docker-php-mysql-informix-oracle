dd9d98ed08aa70a0b4a0b2a29a8c45c652b52121
YOo Slim <slimani.youcef.09@gmail.com>
2018-08-30 10:51:35 +0100

    Test PDO DSN

--- php-app/index.php
+++ php-app/index.php
@@ -1,3 +1,42 @@
 <?php
 
-phpinfo();
+/*
+ * ORACLE DSN WITH PDO TEST
+ * */
+
+$oracle = [
+    'host'      => '127.0.0.1',
+    'database'  => 'test_database',
+    'username'  => 'test_user',
+    'password'  => 'test_password'
+];
+
+
+try {
+    $conn = new PDO('oci:dbname=//' . $oracle['host'] . '/' . $oracle['database'], $oracle['username'], $oracle['password']);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+
+/*
+ * INFORMIX DSN WITH PDO TEST
+ * */
+
+$informix = [
+    'host'      => '127.0.0.1',
+    'service'   => '9091',
+    'server'    => 'test_server_name',
+    'database'  => 'test_database',
+    'protocol'  => 'olsoctcp',
+    'username'  => 'test_user',
+    'password'  => 'test_password',
+];
+
+try {
+    $dbh = new PDO('informix:host=' . $informix['host'] . ';service=' . $informix['service'] . ';server=' . $informix['server'] . ';database=' . $informix['database'] . ';protocol=' . $informix['protocol'] . ';', $informix['username'], $informix['password']);
+}
+catch (PDOException $e)
+{
+    echo $e->getMessage();
+}
